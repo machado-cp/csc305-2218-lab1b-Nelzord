d5b8c197d6601a9420a54f96c9b10d798896db0c
nelzord <jmn3lson@gmail.com>
2021-09-21 12:50:22 -0700

    debugged thesarus

--- src/edu/calpoly/csc305/debugging/DebuggerMain.java
+++ src/edu/calpoly/csc305/debugging/DebuggerMain.java
@@ -3,7 +3,6 @@ package edu.calpoly.csc305.debugging;
 public class DebuggerMain {
   public static void main(String[] args) {
     Thesaurus thesaurus = new Thesaurus();
-
     thesaurus.addAlternatives("interesting",
          "absorbing", "consuming", "engaging", "fascinating");
 
--- src/edu/calpoly/csc305/debugging/Thesaurus.java
+++ src/edu/calpoly/csc305/debugging/Thesaurus.java
@@ -4,16 +4,27 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.SynchronousQueue;
 
 public class Thesaurus {
   private Map<String, List<String>> synonyms;
 
+
   public Thesaurus() {
     synonyms = new HashMap<>();
+    //synonyms.put("interesting", Arrays.asList());
   }
 
+  //check if there is a key inside the map and if not it is put instead of called
   public void addAlternatives(String word, String ... alternatives) {
-    synonyms.get(word).addAll(Arrays.asList(alternatives));
+    if (synonyms.containsKey(word)) {
+      synonyms.get(word).addAll(Arrays.asList(alternatives));
+    } else {
+      synonyms.put(word, Arrays.asList(alternatives));
+    }
+
+
+
   }
 
   public List<String> alternatives(String word) {
